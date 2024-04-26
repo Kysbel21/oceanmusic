ec54f1973514a4970565ae41e870043d7f942f2c
Kysbel Parra <kysbelplara@gmail.com>
2024-04-26 10:55:09 -0400

    casi listo

--- script.js
+++ script.js
@@ -1,98 +1,38 @@
-//String
-const mensaje = 'Instituto Alfa Carabobo'
-
-
-//Number
-const numero = 7.452
-
-
-//Boolean
-const condicion = false
-
-//Aray es una lista de datos que se guarana en memoria de forma sucesiva
-const mercado = [
-    'Tomates',
-    'Lechuga',
-    'Salsa Maíz',
-    'Yogurt',
-    'Pan',
-]
-
-//Object es u tipo de dato que nos permite crear/guardar valores dentro de él pero asignandolé una clave especifica a cada valores
-const usuario = {
-    email: 'pedrooutlook',
-    password: '1234*',
-    username: 'Pedro54',
-    address: {
-        city: 'Valencia',
-        state: 'Carabobo',
-        street: 'Madison',
-        code: '520',
-    },
-    children: [
-        'Maria',
-        'Ana',
-        'Paola',
-    ]
-
-
-
-}
- 
-const indefinido= undefined
-
-const vacio= null
-
-/**
- * OPERADORES LÓGICOS
- * 
- * &&     AND o también y
- * ||     OR o también o
- * =    Asignación
- * ==   Comparación igual
- * !=   desigualdad
- * <    menor
- * <=   menor o igual
- * >    mayor
- * >=   mayor o igual
- * 
- * 
- * OPERADORES MATEMÁTICOS
- * +   suma o adicion
- * -   resta o sustracción 
- * *   multiplicación
- * /   división
- * %   ?
- */
-
-
-
-// if (1==1) {
-//     console.log("se cumple")
-// }
-// else {
-//     console.log("no se cumple")
-// }
-
-
-
-
-// console.log(usuario.Address.code)
-
-// Una función es un bloque de código que contiene una serie de instrucciones o sentencias
-// Conatante o variable es un espacio en memoria que vamos a utilizar después 
-// Una función recibe (parametros): son como variables que existen dentro de la función 
-
-
-const suma = (num1, num2) => {
-    return num1 + num2
-}
-
-const elemento= document.getElementById('identify')
-console.log(elemento)
-
 axios.get("https://leonardoapi.onrender.com/songs")
 
-.then((data) => {
-    console.log(data)
-})
+    .then(
+        (res) => {
+            const contenedor = document.getElementById('track-list')
+
+            //recorrer las canciones//
+            res.data.songs.map(
+                (song) => {
+                    //esto se repite por cada canción
+
+                    const songHtml = document.createElement('div')
+                    songHtml.classList.add('main-content__item')
+
+                    songHtml.innerHTML = `
+                            <img src="${song.path.front}" alt="Portada de una canción">
+                            <div>
+                                <p>${song.title}</p>
+                                <p>${song.author}</p>
+                            </div>
+                    `
+
+                    songHtml.addEventListener('click', () =>{
+                        document.getElementById('current-song-audio').setAttribute('src', song.path.audio)
+                        document.getElementById('current-song-img').setAttribute('src', song.path.front)
+                        document.getElementById('current-song-title').innerHTML = song.title
+                    })
+
+
+                    contenedor.appendChild(songHtml)
+
+
+                }
+            )
+        }
+
+
+    )
