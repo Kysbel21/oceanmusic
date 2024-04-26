0dc5a76dc47ecabd6fa5b39170fdf1bedc279f44
Kysbel Parra <kysbelplara@gmail.com>
2024-04-26 11:38:05 -0400

    listo

--- script.js
+++ script.js
@@ -24,6 +24,7 @@ axios.get("https://leonardoapi.onrender.com/songs")
                         document.getElementById('current-song-audio').setAttribute('src', song.path.audio)
                         document.getElementById('current-song-img').setAttribute('src', song.path.front)
                         document.getElementById('current-song-title').innerHTML = song.title
+                        document.getElementById('current-song-author').innerHTML = song.author
                     })
 
 
